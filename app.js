36b717c278d0f3a84e5ca24566d4204b6446d010
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-06 12:53:03 +0100

    code added

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const gameSummary = {
-    numbers: 0,
-    wins: 0,
-    losses: 0,
-    draws: 0,
-   }
-   
+	numbers: 0,
+	wins: 0,
+	losses: 0,
+	draws: 0,
+};
+
+$(function () {});
+const hand = ['rock', 'paper', 'scissors'];
+function rock() {
+	$('#player').html("<i class='symbol far fa-hand-rock'></i>");
+	rockPaperScissors();
+}
