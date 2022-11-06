1054f0d28b05f8107f4f6e76b53db69572547181
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-11-06 12:54:16 +0100

    code added

--- app.js
+++ app.js
@@ -21,3 +21,40 @@ function scissors() {
 	$('#player').html("<i class='symbol far fa-hand-scissors'></i>");
 	rockPaperScissors();
 }
+
+function rockPaperScissors() {
+	var symbol = arguments.callee.caller.name;
+	var cpuSymbol = hand[Math.floor(Math.random() * 3)];
+	switch (cpuSymbol) {
+		case 'rock':
+			$('#computer').html("<i class='symbol far fa-hand-rock'></i>");
+			console.log(
+				symbol == 'scissors'
+					? denoteLoser()
+					: symbol == 'paper'
+					? denoteWinner()
+					: denoteDraw()
+			);
+			break;
+		case 'paper':
+			$('#computer').html("<i class='symbol far fa-newspaper'></i>");
+			console.log(
+				symbol == 'rock'
+					? denoteLoser()
+					: symbol == 'scissors'
+					? denoteWinner()
+					: denoteDraw()
+			);
+			break;
+		case 'scissors':
+			$('#computer').html("<i class='symbol far fa-hand-scissors'></i>");
+			console.log(
+				symbol == 'paper'
+					? denoteLoser()
+					: symbol == 'rock'
+					? denoteWinner()
+					: denoteDraw()
+			);
+			break;
+	}
+}
